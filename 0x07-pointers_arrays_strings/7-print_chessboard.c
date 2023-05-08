9b4df8783c2e4a561d1d4ec0754a8c670cad51c0
Joycewutsika <joycesena86@gmail.com>
2023-05-08 14:05:49 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m;
+	int p;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (p = 0; p < 8; p++)
+			_putchar(a[m][p]);
+		_putchar('\n');
+	}
+}
+
