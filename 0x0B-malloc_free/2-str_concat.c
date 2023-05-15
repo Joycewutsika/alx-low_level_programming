a275d620a5799aa4ae380708646d18d6ad66e7d6
Joycewutsika <joycesena86@gmail.com>
2023-05-15 12:02:14 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,14 +8,15 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int x;
+	int y;
 	char *str;
-	int x = 0, y = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
+	x = y = 0;
 	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
@@ -24,6 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (str == NULL)
 		return (NULL);
+	x = y = 0;
 	while (s1[x] != '\0')
 	{
 		str[x] = s1[x];
