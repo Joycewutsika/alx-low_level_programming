8453f131f7a414be83e0bb2d9ce86dd4d435bc9e
Joycewutsika <joycesena86@gmail.com>
2023-05-15 11:53:45 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,15 +8,15 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int m = 0, n = 0, x = 0, y = 0;
+	int m, n, x = 0, y = 0;
 	char *p;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (; m < ac; m++)
+	for (m = 0; m < ac; m++)
 	{
-		for (; av[m][n]; n++)
+		for (n = 0; av[m][n]; n++)
 			y++;
 	}
 	y += ac;
@@ -24,9 +24,9 @@ char *argstostr(int ac, char **av)
 	p = malloc(sizeof(char) * y + 1);
 	if (p == NULL)
 		return (NULL);
-	for (; m < ac; m++)
+	for (m = 0; m < ac; m++)
 	{
-	for (; av[m][n]; n++)
+	for (n = 0; av[m][n]; n++)
 	{
 		p[x] = av[m][n];
 		x++;
