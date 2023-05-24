c73790533f40e963d00f9405a8f693a5e18fde1c
Joycewutsika <joycesena86@gmail.com>
2023-05-24 07:03:16 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - prints index of first element
+ * @array: array
+ * @size: number of elements
+ * @cmp: pointer to function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
+
