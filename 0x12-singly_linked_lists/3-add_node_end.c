e71957776d04dd356b5d71815b8d5b520abb087e
Joycewutsika <joycesena86@gmail.com>
2023-06-01 15:14:11 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,20 +11,20 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	list_t *now = *head;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = NULL;
-
-	list_t *now = *head;
 
 	if (*head == NULL)
 	{
@@ -39,3 +39,4 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (new_node);
 }
+
