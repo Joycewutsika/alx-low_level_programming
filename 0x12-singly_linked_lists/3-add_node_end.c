17ea63379d9d256b4626e7d00bf61335d7706669
Joycewutsika <joycesena86@gmail.com>
2023-06-01 14:25:39 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	list_t *current = *head;
 
-	while (current->next)
+	while (current->next != NULL)
 		current = current->next;
 
 	current->next = new;
