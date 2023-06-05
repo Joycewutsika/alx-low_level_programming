8cec40eb0d3b0b65912f5d19e2846090467b1acd
Joycewutsika <joycesena86@gmail.com>
2023-06-05 08:54:02 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+  *free_listint_safe - frees a linked list
+  *@h: pointer
+  *
+  *Return: number
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *jay;
+	int less;
+	size_t length = 0;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		less = *h - (*h)->next;
+		if (less > 0)
+		{
+			jay = (*h)->next;
+			free(*h);
+			*h = jay;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (length);
+}
