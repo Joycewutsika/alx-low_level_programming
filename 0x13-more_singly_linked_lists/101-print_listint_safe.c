19089369a0eb6dc0f18d6781cf8927eecea0cbc1
Joycewutsika <joycesena86@gmail.com>
2023-06-05 13:41:42 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,14 +1,62 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t print_listint_safe(const listint_t *head);
 size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
-  *print_listint_safe - prints list safely
-  *@head: pointer
-  *
-  *Return: the number of nodes
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
@@ -24,6 +72,7 @@ size_t print_listint_safe(const listint_t *head)
 			head = head->next;
 		}
 	}
+
 	else
 	{
 		for (index = 0; index < nodes; index++)
@@ -31,54 +80,10 @@ size_t print_listint_safe(const listint_t *head)
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
+
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
 	return (nodes);
 }
-
-/**
-  *looped_listint_len - calculates the length of a linked list
-  *@head: pointer
-  *
-  *Return: 0, or number of unique nodes
-  */
-size_t looped_listint_len(const listint_t *head)
-{
-	size_t nodes;
-	const listint_t *up, *down;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	up = head->next;
-	down = (head->next)->next;
-
-	while (down)
-	{
-		if (up == down)
-		{
-			up = head;
-			while (up != down)
-			{
-				nodes++;
-				up = up->next;
-				down = down->next;
-			}
-
-			up = up->next;
-			while (up != down)
-			{
-				nodes++;
-				up = up->next;
-			}
-			return (nodes);
-		}
-
-		up = up->next;
-		down = (down->next)->next;
-	}
-	return (0);
-}
-
 
