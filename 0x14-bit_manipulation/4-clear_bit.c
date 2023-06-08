733b90cca78fc27a3d39fc6e6a80bf852435ec4d
Joycewutsika <joycesena86@gmail.com>
2023-06-08 08:03:35 -0700

    sets binary at a given index to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *clear_bit - sets bit to 0
+  *@n: number
+  *@index: index
+  *
+  *Return: 1 (success), -1 (failure)
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
